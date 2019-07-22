94b37f9e37fb5642ac0ccff5bc70fe66f193b873
dj278016981@gmail.com <13558665407dj>
2019-07-22 17:43:10 +0800

    队列、栈

--- algorithm/list/Solution2.java
+++ algorithm/list/Solution2.java
@@ -3,10 +3,12 @@ package algorithm.list;
 /**
  * @author dengjia
  * @date 2019/7/16 15:51
+ * 删除所有元素
  */
 public class Solution2 {
     public ListNode removeElements(ListNode head, int val) {
 
+        // 解决头结点是要删除的元素
         while (head != null && head.val == val) {
             ListNode delNode = head;
             head = head.next;
@@ -29,5 +31,15 @@ public class Solution2 {
         }
 
         return head;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution2()).removeElements(head, 6);
+        System.out.println(res);
     }
 }
