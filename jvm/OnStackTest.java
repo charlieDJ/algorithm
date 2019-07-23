61829e57fe265e5c6d647157da71e26d758f05f1
dj278016981@gmail.com <13558665407dj>
2019-07-23 15:39:57 +0800

    逃逸分析

--- jvm/OnStackTest.java
+++ jvm/OnStackTest.java
@@ -1,26 +1,43 @@
 package jvm;
 
+/**
+ * @author dengjia
+ */
 public class OnStackTest {
-    // User类
-    public static class User{
-        public int id=0;
-        public String name="";
+    /**
+     * User类
+     */
+    public static class User {
+        public int id = 0;
+        public String name = "";
     }
-    // 创建User类对象
-    public static void alloc(){
-        User u=new User();
-        u.id=5;
-        u.name="geym";
+
+    /**
+     * 创建User类对象
+     */
+    public static void alloc() {
+        User u = new User();
+        u.id = 5;
+        u.name = "geym";
     }
-    // 程序入口
+
+    /**
+     * 开启逃逸分析
+     * -Xmx5m -Xms5m -XX:+PrintGC -XX:+DoEscapeAnalysis -XX:+UseTLAB -XX:+EliminateAllocations
+     * 关闭逃逸分析
+     * -Xmx5m -Xms5m -XX:+PrintGC -XX:-DoEscapeAnalysis -XX:-UseTLAB -XX:-EliminateAllocations
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
-        long b=System.currentTimeMillis();
+        long b = System.currentTimeMillis();
         // 创建大量的对象
-        for(int i=0;i<99999999;i++){
+        int length = 99999999;
+        for (int i = 0; i < length; i++) {
             alloc();
         }
-        long e=System.currentTimeMillis();
+        long e = System.currentTimeMillis();
         // 打印执行时间
-        System.out.println(e-b);
+        System.out.println(String.format("执行时间：%d ms", e - b));
     }
 }
