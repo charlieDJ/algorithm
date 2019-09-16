742d32b029983a132253f60644b7d121548f4538
dj278016981@gmail.com <13558665407dj>
2019-09-16 17:18:07 +0800

    参数修改

--- jvm/OnStackTest.java
+++ jvm/OnStackTest.java
@@ -10,6 +10,22 @@ public class OnStackTest {
     public static class User {
         public int id = 0;
         public String name = "";
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
     /**
@@ -17,15 +33,17 @@ public class OnStackTest {
      */
     public static void alloc() {
         User u = new User();
-        u.id = 5;
-        u.name = "geym";
+        u.setId(5);
+        u.setName("geym");
     }
 
     /**
      * 开启逃逸分析
-     * -Xmx5m -Xms5m -XX:+PrintGC -XX:+DoEscapeAnalysis -XX:+UseTLAB -XX:+EliminateAllocations
+     * -Xmx5m -Xms5m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+DoEscapeAnalysis -XX:+UseTLAB -XX:+EliminateAllocations
+     * -Xmx5m -Xms5m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+DoEscapeAnalysis -XX:+UseTLAB -XX:+EliminateAllocations
      * 关闭逃逸分析
-     * -Xmx5m -Xms5m -XX:+PrintGC -XX:-DoEscapeAnalysis -XX:-UseTLAB -XX:-EliminateAllocations
+     * -Xmx5m -Xms5m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:-DoEscapeAnalysis -XX:-UseTLAB -XX:-EliminateAllocations
+     * -Xmx5m -Xms5m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:-DoEscapeAnalysis -XX:-UseTLAB -XX:-EliminateAllocations
      * @param args
      * @throws InterruptedException
      */
@@ -38,6 +56,6 @@ public class OnStackTest {
         }
         long e = System.currentTimeMillis();
         // 打印执行时间
-        System.out.println(String.format("执行时间：%d ms", e - b));
+        System.err.println(String.format("执行时间：%d ms", e - b));
     }
 }
