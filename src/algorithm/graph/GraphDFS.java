721cbbc21ad39ae08ec668a03ec07b954cb0a7f7
dengjia <278016981@qq.com>
2020-04-05 21:15:34 +0800

    联通分量和后序遍历

--- src/algorithm/graph/GraphDFS.java
+++ src/algorithm/graph/GraphDFS.java
@@ -9,9 +9,13 @@ import java.util.List;
 public class GraphDFS {
 
     /**
-     * 存放顶点的列表
+     * 先序遍历存放顶点的列表
      */
-    private List<Integer> order;
+    private List<Integer> pre;
+    /**
+     * 后序遍历存放顶点的列表
+     */
+    private List<Integer> post;
     /**
      * 记录索引处的顶点是否被访问
      */
@@ -23,15 +27,21 @@ public class GraphDFS {
 
     public GraphDFS(Graph G) {
         this.G = G;
-        order = new ArrayList<>();
+        pre = new ArrayList<>();
+        post = new ArrayList<>();
         // 空间等于图的顶点数
         visited = new boolean[G.V()];
-        dfs(0);
+        // 针对联通分量做出的改进，每个顶点都需要遍历
+        for (int v = 0; v < G.V(); v++) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
     }
 
     private void dfs(int v) {
         visited[v] = true;
-        order.add(v);
+        pre.add(v);
         //遍历与此顶点相邻的顶点
         for (Integer w : G.adj(v)) {
             // 如果遍历过，不再重复遍历。因为这些节点可能存在环
@@ -39,19 +49,28 @@ public class GraphDFS {
                 dfs(w);
             }
         }
+        post.add(v);
     }
 
     /**
      * @return 返回遍历到的顶点
      */
-    public Iterable<Integer> order() {
-        return order;
+    public Iterable<Integer> pre() {
+        return pre;
+    }
+
+    /**
+     * @return 返回后序遍历到的顶点
+     */
+    public Iterable<Integer> post() {
+        return post;
     }
 
     public static void main(String[] args) {
         final Graph G = new Graph("g1.txt");
         final GraphDFS graphDFS = new GraphDFS(G);
-        System.out.println(graphDFS.order());
+        System.out.println(graphDFS.pre());
+        System.out.println(graphDFS.post());
     }
 
 }
