de107c42ec8823e2d8579f7d252aeda43bcc8431
dj <dj278016981@gmail.com>
2020-05-05 20:55:25 +0800

    AVL 删除节点

--- src/algorithm/avl/AVLTree.java
+++ src/algorithm/avl/AVLTree.java
@@ -252,13 +252,14 @@ public class AVLTree<K extends Comparable<K>, V> {
         if (node == null) {
             return null;
         }
-
+        // 保存返回节点，后面需要调整高度，达到自平衡的效果
+        Node retNode = null;
         if (key.compareTo(node.key) < 0) {
             node.left = remove(node.left, key);
-            return node;
+            retNode = node;
         } else if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
-            return node;
+            retNode = node;
         } else {   // key.compareTo(node.key) == 0
 
             // 待删除节点左子树为空的情况
@@ -266,29 +267,59 @@ public class AVLTree<K extends Comparable<K>, V> {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
+                retNode = rightNode;
             }
 
             // 待删除节点右子树为空的情况
-            if (node.right == null) {
+            else if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
+            } else {
+                // 待删除节点左右子树均不为空的情况
+
+                // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+                // 用这个节点顶替待删除节点的位置
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.key);
+                successor.left = node.left;
+
+                node.left = node.right = null;
+
+                retNode = successor;
             }
 
-            // 待删除节点左右子树均不为空的情况
-
-            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
-            // 用这个节点顶替待删除节点的位置
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-
-            node.left = node.right = null;
-
-            return successor;
         }
+
+        if (retNode == null) {
+            return null;
+        }
+
+        // 更新height
+        retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+        // 左子树高度大于右子树，且左子树上的节点大于右子树
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0) {
+            return rightRotate(retNode);
+        }
+        // 同样，查看是否需要左旋转
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0) {
+            return leftRotate(retNode);
+        }
+        // LR
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+        // RL
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+        return retNode;
     }
 
     public static void main(String[] args) {
@@ -313,7 +344,14 @@ public class AVLTree<K extends Comparable<K>, V> {
 
             System.out.println("is BST: " + map.isBST());
             System.out.println("is Balanced: " + map.isBalanced());
+            for (String word : words) {
+                map.remove(word);
+                if (!map.isBST() || !map.isBalanced()) {
+                    throw new RuntimeException("Error");
+                }
+            }
         }
+
 
         System.out.println();
     }
